769af9e235cefae0c646876f2938b75aeca8faa4
Abdillah Mufki Auzan Mubin <abdillahmufky123@gmail.com>
2023-01-10 23:37:45 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className={"bg-blue-600 grid place-content-center min-h-screen"}>
+      <h1 className={"text-xl"}>Hello World!</h1>
+    </div>
+  );
+}
